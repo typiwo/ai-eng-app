76550e9c042bb1079e3b3deb344aa486575fd5e9
typiwo <tpiwowarski3@gatech.edu>
2025-02-16 13:26:26 -0500

    Reorganize hello.py into src folder and fix import paths

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,8 +1,7 @@
 import pytest
-from hello import say_hello
+from src.hello import say_hello
 
 def test_say_hello_returns_correct_string():
     expected = "Hello, World!"
     result = say_hello()
     assert expected == result, f"Expected: {expected}, but got: {result}"
-    
